0a2c469b19dc59b7c30e585b99ae3ceb0c6f6038
Sikelor <cc27hddvp9@privaterelay.appleid.com>
2026-08-26 22:10:57 +0200

    Update app.py

--- app.py
+++ app.py
@@ -66,5 +66,5 @@ with gr.Blocks(theme=gr.themes.Soft()) as demo:
     msg.submit(rispondi, [msg, stato_chat], [msg, chatbot])
     clear.click(lambda: ([], []), None, [chatbot, stato_chat])
 
-server_port = int(os.environ.get.("PORT", 7860))
-demo.launch(server_name="0.0.0.0", server_port=server_port)
+server_port = int(os.environ.get.("PORT",7860))
+demo.launch(server_name="0.0.0.0",server_port=server_port)
